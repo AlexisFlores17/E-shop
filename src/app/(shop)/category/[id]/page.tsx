1bfe51077700b941e414d6776845e4b753ad7e59
AlexisFlores17 <alex.p.flores17@gmail.com>
2024-02-02 19:17:42 -0600

    Ui hasta Selected sizes

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,25 +1,39 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces/product.interface";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 
 interface Props {
   params:{
-    id: string
+    id: Category
   }
 }
 
+const seedProducts = initialData.products;
+
+
 
 export default function CategoryPage({params}:Props) {
-
-  const {id} = params;
   
-  if(id === "kids"){
-    notFound();
+  const {id} = params;
+  const products = seedProducts.filter( producto => producto.gender === id);
+  
+
+  const labels: Record<Category, string>= {
+    "men":"para Hombres",
+    "women":"para Mujeres",
+    "kid":"para Niños",
+    "unisex": "para todos"
   }
+  // if(id === "kids"){
+  //   notFound();
+  // }
 
   return (
-    <div>
-      
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title className="mb-2" title={`Articulos ${labels[id]}`} subtitle="Temporal" />
+      <ProductGrid products={products} />
+    </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
+export { SizeSelector } from "./product/size-selector/SizeSelector";
 export { PageNotFound } from "./ui/not-found/PageNotFound";
 export { ProductGrid } from "./products/product-grid/ProductGrid";
 export { ProductGridItem } from "./products/product-grid/ProductGridItem";
 export { Title } from "./ui/title/Title";
 export { TopMenu } from "./ui/top-menu/TopMenu";
-import { SideBar } from "./ui/sidebar/SideBar";
+export { SideBar } from "./ui/sidebar/SideBar";
