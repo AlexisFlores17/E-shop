228c12e923df329f65e9db2d77825941260be509
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2024-02-21 17:41:32 -0600

    feat: :sparkles: Cart in localstorage
    Se implementó la funcion de guardar en local storage con zustand

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -2,51 +2,51 @@ import type { CartProduct } from "@/interfaces/product.interface";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-
 interface State {
-    cart: CartProduct[]
+  cart: CartProduct[];
 
-    addProductToCart : (product:CartProduct) => void
-    // updateProductQuantity
-    // removeProduct
+  addProductToCart: (product: CartProduct) => void;
+  // updateProductQuantity
+  // removeProduct
 }
 
+export const useCartStore = create<State>()(
+    persist(
+        (set, get) => ({
+            cart: [],
 
+            // Method
 
-export const useCartStore = create<State>()( 
+            addProductToCart: (product: CartProduct) => {
+                const { cart } = get();
+                console.log(cart);
+                // 1. Revisar si el producto existe en el carrito con la talla seleccionada
 
-    (set,get) =>({
-    
-        cart:[],
+                const productInCart = cart.some(
+                (item) => item.id === product.id && item.size === product.size
+                );
 
-
-        // Method
-
-        addProductToCart:(product: CartProduct) =>{
-            const {cart} = get()
-            console.log(cart)
-            // 1. Revisar si el producto existe en el carrito con la talla seleccionada
-
-            const productInCart = cart.some(
-                (item)=> item.id === product.id && item.size === product.size
-            );
-
-            if ( !productInCart) {
-                set({ cart:[...cart, product] })
+                if (!productInCart) {
+                set({ cart: [...cart, product] });
                 return;
-            }
+                }
 
-            // 2. Se que el producto existe por talla, incrementar
+                // 2. Se que el producto existe por talla, incrementar
 
-            const updatedCartProducts = cart.map( (item)=>{
-                if( item.id === product.id && item.size === product.size) {
-                    return {...item, quantity: item.quantity + product.quantity}
+                const updatedCartProducts = cart.map((item) => {
+                if (item.id === product.id && item.size === product.size) {
+                    return { ...item, quantity: item.quantity + product.quantity };
                 }
 
                 return item;
-            })
+                });
 
-            set({cart: updatedCartProducts})
+                set({ cart: updatedCartProducts });
+            },
+        }),
+
+        {
+        name: "shopping-cart",
         }
-    })
-)
+    )
+);
