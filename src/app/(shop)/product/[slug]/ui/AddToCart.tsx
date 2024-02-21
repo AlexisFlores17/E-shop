60d40f735907c78d941c022fa2c4a70c2db25604
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2024-02-20 19:22:32 -0600

    feat: :sparkles: Not selected size message
    Se implementó un mensaje de error en caso de que se quiera agregar un producto sin talla

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,38 +1,44 @@
 "use client";
 
-import { QuantitySelector, SizeSelector } from '@/components'
-import { Product } from '@/interfaces'
-import React, { useState } from 'react'
-import { Size } from '../../../../../interfaces/product.interface';
+import { QuantitySelector, SizeSelector } from "@/components";
+import { Product } from "@/interfaces";
+import React, { useState } from "react";
+import { Size } from "../../../../../interfaces/product.interface";
 
 interface Props {
-    product:Product
+  product: Product;
 }
 
-export const AddToCart = ({product}:Props) => {
+export const AddToCart = ({ product }: Props) => {
+  const [size, setSize] = useState<Size | undefined>();
+  const [quantity, setQuantity] = useState<number>(1);
+  const [posted, setPosted] = useState(false);
 
-    const [size, setSize] = useState<Size| undefined >()
-    const [quantity, setQuantity] = useState<number>(1)
+  const addToCart = () => {
+    setPosted(true);
+    if (!size) return;
+    console.log(size, quantity);
+  };
 
-    const addToCart = ()=>{
-        if (!size) return;
-        console.log(size,quantity)
-    }
+  return (
+    <>
+      {posted && !size && (
+        <span className="mt-2 text-red-500 fade-in">Debe de seleccionar una talla</span>
+      )}
 
-    return (
-        <>
-        {/*Selector de tallas*/}
-        <SizeSelector
-            selectedSize={size}
-            availableSizes={product.sizes}
-            onSizeChanged={setSize}
-        />
-        {/*Selector de Cantidad*/}
-        <QuantitySelector  quantity={quantity} onQuantityChanged={setQuantity}  />
-        {/*Button*/}
+      {/*Selector de tallas*/}
+      <SizeSelector
+        selectedSize={size}
+        availableSizes={product.sizes}
+        onSizeChanged={setSize}
+      />
+      {/*Selector de Cantidad*/}
+      <QuantitySelector quantity={quantity} onQuantityChanged={setQuantity} />
+      {/*Button*/}
 
-        <button onClick={addToCart} className="btn-primary my-5">Agregar al carrito</button>
-        
-        </>
-    )
-}
+      <button onClick={addToCart} className="btn-primary my-5">
+        Agregar al carrito
+      </button>
+    </>
+  );
+};
