ddc517e7404cea5c7e6c3acd61141756387dd1f6
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2024-02-20 18:30:08 -0600

    perf: :zap: Client component in selected Size
    Se cambió el metodo de renderizado en un componente para que sea client side

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -11,6 +11,7 @@ import {
 import { titleFont } from "@/config/fonts";
 import type { Metadata, ResolvingMetadata } from 'next'
 import { notFound } from "next/navigation";
+import { AddToCart } from "./ui/AddToCart";
 
 interface Props {
   params: {
@@ -75,16 +76,7 @@ export default async function ProductPage({ params }: Props) {
         </h1>
         <p className="text-lg mb-5">${product.price}</p>
 
-        {/*Selector de tallas*/}
-        <SizeSelector
-          selectedSize={product.sizes[1]}
-          availableSizes={product.sizes}
-        />
-        {/*Selector de Cantidad*/}
-        <QuantitySelector quantity={2} />
-        {/*Button*/}
-
-        <button className="btn-primary my-5">Agregar al carrito</button>
+        <AddToCart product={product} />
 
         {/*Descripción*/}
         <h3 className="font-bold text-sm ">Descripción</h3>
---
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { QuantitySelector, SizeSelector } from '@/components'
+import { Product } from '@/interfaces'
+import React, { useState } from 'react'
+import { Size } from '../../../../../interfaces/product.interface';
+
+interface Props {
+    product:Product
+}
+
+export const AddToCart = ({product}:Props) => {
+
+    const [size, setSize] = useState<Size| undefined >()
+
+    return (
+        <>
+        {/*Selector de tallas*/}
+        <SizeSelector
+          selectedSize={size}
+          availableSizes={product.sizes}
+          onSizeChanged={setSize}
+        />
+        {/*Selector de Cantidad*/}
+        <QuantitySelector quantity={2} />
+        {/*Button*/}
+
+        <button className="btn-primary my-5">Agregar al carrito</button>
+        
+        </>
+    )
+}
