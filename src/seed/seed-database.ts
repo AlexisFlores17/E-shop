7d3198b1886f5920181678abdd88740789a1e1dd
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2024-03-08 19:14:08 -0600

    fix: :bug: Fix seed order deleted
    Se corrigio la logica para borrar de la base de datos las ordenes con sus relaciones

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -5,12 +5,16 @@ import { countries } from "./seed-countries";
 async function main() {
   //Borrar registros previos
   // await Promise.all([
+    await prisma.orderAddress.deleteMany();
+    await prisma.orderItem.deleteMany();
+    await prisma.order.deleteMany();
+    
     await prisma.userAddress.deleteMany();
+    await prisma.user.deleteMany();
     await prisma.country.deleteMany();
     await prisma.productImage.deleteMany();
     await prisma.product.deleteMany();
     await prisma.category.deleteMany();
-    await prisma.user.deleteMany();
   // ]);
 
   const { categories, products, users } = initialData;
