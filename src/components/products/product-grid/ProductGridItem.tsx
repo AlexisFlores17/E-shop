59ae999dc257ef7a064ca970cc43ad59f9cb23c8
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2024-05-01 01:17:43 -0600

    Update ProductGridItem.tsx

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -19,7 +19,7 @@ export const ProductGridItem = ({ product }: Props) => {
           priority={true}
           onMouseLeave={()=>setDisplayImage(product.images[0])}
           onMouseEnter={()=>setDisplayImage(product.images[1])}
-          src={`/products/${displayImage} `}
+          src={"/imgs/placeholder.jpg"}
           alt={product.title}
           className="w-full object-cover rounded"
           width={500}
