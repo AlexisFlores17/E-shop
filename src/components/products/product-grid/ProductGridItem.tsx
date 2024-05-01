f9c6e09cd8eb83728f515aaa132bc6f4acfa0f37
AlexisFlores17 <35354528+AlexisFlores17@users.noreply.github.com>
2024-05-01 01:34:46 -0600

    Update ProductGridItem.tsx

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -9,15 +9,15 @@ interface Props {
 }
 
 export const ProductGridItem = ({ product }: Props) => {
-  const [displayImage, setDisplayImage] = useState(product.images[0])
+  const [displayImage, setDisplayImage] = useState(product.images[1])
 
   
   return (
     <div className="rounded-md overflow-hidden fade-in">
       <Link href={`/product/${product.slug}`}>
         <ProductImage
-          onMouseLeave={()=>setDisplayImage(product.images[0])}
-          onMouseEnter={()=>setDisplayImage(product.images[1])}
+          // onMouseLeave={()=>setDisplayImage(product.images[0])}
+          // onMouseEnter={()=>setDisplayImage(product.images[1])}
           src={`/products/${displayImage}`}
           alt={product.title}
           className="w-full object-cover rounded"
